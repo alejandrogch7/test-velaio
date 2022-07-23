800020a708068a351d2b09a86cd7b85885e52361
Alex <alexdev.gc@gmail.com>
2022-07-23 15:53:01 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useAuth0 } from '@auth0/auth0-react'
 import axios from 'axios'
 import LoginButton from './components/Login'
 import LogoutButton from './components/Logout'
-import Profile from './components/profile'
+import Profile from './components/Profile'
 import Greeting from './components/Greeting'
 
 
