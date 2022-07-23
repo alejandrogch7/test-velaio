e2c08cb3406424bb57da8bd888a44609f9cd68aa
Alex <alexdev.gc@gmail.com>
2022-07-23 16:36:52 -0500

    fourth commit

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 const Greeting = () => {
   return (
-    <div>
-      <h1>
-        Hello! please Log in or Sign in to have access to the app
-      </h1>
+    <div className='containerGreeting'>
+      <h1> Hello! please Log in or Sign in to have access to the app </h1>
     </div>
   )
 }
